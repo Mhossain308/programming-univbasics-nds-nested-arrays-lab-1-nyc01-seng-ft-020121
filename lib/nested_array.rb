15f890001eb2e6c9fb066993d041d7dd34e9efa4
Mohamed Hossain <mz.hossain212@gmail.com>
2021-01-21 16:31:09 +0000

    Automatically backed up by Learn

--- lib/nested_array.rb
+++ lib/nested_array.rb
@@ -40,6 +40,6 @@ def matrix_update(matrix, row, column, new_value)
     ["Eggplant", "Grapefruit", "Oranges", "Pineapple", "Watermelon"],
     ["Asparagus", "Avocadoes", "Grapes", "Potatoes", "Strawberries"]
   ]
-matrix[1][1] = "Onions"
-matrix_update
+matrix[row][column] = "new_value"
+
 end
